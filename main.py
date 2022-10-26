79ca2161edaf13318eead1e7312e5d19c2dcc5a4
Luca Silly <luca.silly@etu.unige.ch>
2022-10-26 14:48:03 +0200

    google form

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+from __future__ import print_function
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
+
 import os
 import discord
 from discord import app_commands
 from dotenv import load_dotenv
+
+
+
+
+
+SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
+DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+
+store = file.Storage('token.json')
+creds = None
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = discovery.build('forms', 'v1', http=creds.authorize(
+    Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
+
+# Prints the responses of your specified form:
+form_id = '1NajjW2LOQ40qk_AQ1Okm3g4LAbW2ohk-SAk8o6VAXog'
+result = service.forms().responses().list(formId=form_id).execute()
+print(result)
+
+
 
 load_dotenv(dotenv_path="config")
 
